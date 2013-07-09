923410809b7dda59ed13b9d7014fb2fd94ad33cc
Pål Jørgensen <pal.jorgensen@amendor.com>
2013-07-09 18:49:40 +0200

    OPPG-416 - fixes after review

--- scripts/eta.js
+++ scripts/eta.js
@@ -1,175 +1,182 @@
 var H5P = H5P || {};
 
-/**
- * Constructor.
- * 
- * @param {object} params Options for this library.
- * @param {string} contentPath The path to our content folder.
- */
-H5P.ExportableTextArea = function (params, contentPath) {
-  var self = this;
-  
-  console.log(params);
-  
-  self.index = (params.index !== undefined ? params.index+1 : '');
-  self.header = '<span class="index">' + self.index + '</span>. <span class="label">' + (params.label ? params.label : '') +'</span>';
-  
-  var attach = function ($wrapper) {
-    var supported = H5P.ExportableTextArea.Exporter.supportsExport();
-    var classes = 'h5p-eta-input' + (supported ? '' : ' not-supported');
-    var text = supported ? '' : 'Not supported on your device';
-    self.$content = H5P.jQuery('<div class="h5p-eta-label">'+self.header+'</div><textarea '+(supported ? '' : 'disabled="disabled"')+' class="'+classes+'" data-index="'+self.index+'">'+text+'</textarea>');
-    $wrapper.addClass('h5p-eta').html(self.$content);
+H5P.ExportableTextArea = (function ($) {
+  /**
+   * Constructor.
+   *
+   * @param {object} params Options for this library.
+   * @param {int} id Content identifier
+   */
+  function C(params, id) {    
+    this.index = (params.index !== undefined ? params.index + 1 : '');
+    this.header = '<span class="index">' + this.index + '</span>. <span class="label">' + (params.label ? params.label : '') + '</span>';
   };
   
-  var onDelete = function(params, slideIndex, elementIndex) {
-    H5P.ExportableTextArea.CPInterface.onDelete(params, slideIndex, elementIndex, self);
-  };
-  
-  var onAdd = function(params, slideIndex) {
-    H5P.ExportableTextArea.CPInterface.onAdd(params, slideIndex, self);
-  };
-  
-  return {
-    attach: attach,
-    machineName: 'H5P.ExportableTextArea',
-    exportAnswers: true,
-    onDelete: onDelete,
-    onAdd: onAdd
-  };
-};
-
-/**
- * Interface responsible for handling index calculations beeing done when 
- * adding and removing Answer elements 
- */
-H5P.ExportableTextArea.CPInterface = (function() {
- 
-  /* Containing number of Answer elements per slide */
-  var answerCounter = [];
-  
-  return {
-    onDelete: function(params, slideIndex, elementIndex, elementInstance) {
-      // Reorder index on current slide
-      var filtered = params[slideIndex].elements.filter(function(element, index){
-        return H5P.libraryFromString(element.action.library).machineName === 'H5P.ExportableTextArea';
-      }).sort(function(a,b){
-        return b.action.params.index - a.action.params.index;
-      });
-      
-      answerCounter[slideIndex] = [];
-      for (var i = 0; i<filtered.length; i++) {
-        filtered[i].action.params.index = i;
-        answerCounter[slideIndex][i] = true;
-        H5P.jQuery('.h5p-slides-wrapper > .h5p-current').children('.h5p-eta').eq(i).find('.index').html(i+1);
-      }
-    },
-    onAdd: function(params, slideIndex, elementInstance) {
-      if(answerCounter[slideIndex] === undefined) {
-        answerCounter[slideIndex] = [];
-      }
-      if(params.action.params.index === undefined) {
-        params.action.params.index = answerCounter[slideIndex].length;
-      }
-      
-      console.log(answerCounter[slideIndex].length);
-      
-      answerCounter[slideIndex][params.action.params.index] = true;
+  C.prototype.attach = function ($wrapper) {
+    if(H5P.ExportableTextArea.Exporter.supportsExport()) {
+      this.$content = H5P.jQuery('<div class="h5p-eta-label">' + this.header + '</div><textarea class="h5p-eta-input" data-index="' + this.index + '"></textarea>');
+      $wrapper.addClass('h5p-eta').html(this.$content);
+    }
+    else {
+      $wrapper.addClass('not-supported');
     }
   };
+
+  C.prototype.onDelete = function (params, slideIndex, elementIndex) {
+    H5P.ExportableTextArea.CPInterface.onDelete(params, slideIndex, elementIndex, this);
+  };
+
+  C.prototype.onAdd = function (params, slideIndex) {
+    H5P.ExportableTextArea.CPInterface.onAdd(params, slideIndex, this);
+  };
+  
+  C.prototype.exportAnswers = true;
+
+  return C;
+})(H5P.jQuery);
+
+/**
+ * Interface responsible for handling index calculations beeing done when
+ * adding and removing Answer elements
+ * 
+ * Implemented as singleton
+ */
+H5P.ExportableTextArea.CPInterface = (function () {
+  if ( arguments.callee._singletonInstance ) {
+    return arguments.callee._singletonInstance;
+  }
+  arguments.callee._singletonInstance = this;
+    
+  this.answerCounter = [];
+  
+  this.onDelete = function (params, slideIndex, elementIndex, elementInstance) {
+    // Reorder index on current slide
+    var filtered = params[slideIndex].elements.filter(function (element, index) {
+      return H5P.libraryFromString(element.action.library).machineName === 'H5P.ExportableTextArea';
+    }).sort(function (a, b) {
+      return b.action.params.index - a.action.params.index;
+    });
+
+    this.answerCounter[slideIndex] = [];
+    for (var i = 0; i < filtered.length; i++) {
+      filtered[i].action.params.index = i;
+      this.answerCounter[slideIndex][i] = true;
+      H5P.jQuery('.h5p-slides-wrapper > .h5p-current').children('.h5p-eta').eq(i).find('.index').html(i + 1);
+    }
+  };
+  
+  this.onAdd = function (params, slideIndex, elementInstance) {
+    if (this.answerCounter[slideIndex] === undefined) {
+      this.answerCounter[slideIndex] = [];
+    }
+    if (params.action.params.index === undefined) {
+      params.action.params.index = this.answerCounter[slideIndex].length;
+    }
+
+    this.answerCounter[slideIndex][params.action.params.index] = true;  
+  };
+  
+  return this;
 })();
 
 /**
  * Export all Answers
+ * 
+ * Implemented as singleton
  */
-H5P.ExportableTextArea.Exporter = (function() {
+H5P.ExportableTextArea.Exporter = (function () {
+
+  if ( arguments.callee._singletonInstance ) {
+    return arguments.callee._singletonInstance;
+  }
+  arguments.callee._singletonInstance = this;
   
-  var deviceIsIPx = undefined;
-  var useFlash = undefined;
-  
-  return {
-    run: function() {
-      // Save it as a file:
-      if (this.useFileSaver()){
-        var blob = new Blob([this.createDocContent()], {type: "application/msword;charset=utf-8"});
-        saveAs(blob, 'exported-text.doc');
-      }
-    },
-    
-    createDocContent: function() {
-      // Create HTML:
-      var html = '<html><body>';
-      H5P.jQuery('.h5p-slide').each(function(index){
-        // Sort on index per slide, then create html
-        H5P.jQuery('.h5p-eta-input', this).sort(function(a,b){
-          return H5P.jQuery(a).data('index') > H5P.jQuery(b).data('index') ? 1 : -1;
-        }).each(function(){
-          html += '<h2>' + H5P.jQuery(this).prev().find('.label').text() + '</h2>';
-          html += '<p>' + H5P.jQuery(this).val() + '</p>';
-        });
+  this.deviceIsIPx = undefined;
+  this.useFlash = true;//undefined;
+
+  this.run = function () {
+    // Save it as a file:
+    if (this.useFileSaver()) {
+      var blob = new Blob([this.createDocContent()], {
+        type: "application/msword;charset=utf-8"
       });
-      html += '</body></html>';
-      
-      return html;
-    },
-    
-    createExportButton: function(title) {
-      var self = this;
-      
-      if(!H5P.ExportableTextArea.Exporter.supportsExport()) {
-        return '';
-      }
-      
-      if(this.useFileSaver()) {
-        return '<a href="javascript:void(0)" class="h5p-eta-export" style="display: none;">' + title + '</a>';
-      }
-      else {
-        var $downloadify = $('<div></div>');
-        $downloadify.appendTo('body');
-        
-        $downloadify.downloadify({
-          filename: function(){
-            return 'answer-text.doc';
-          },
-          data: function(){ 
-            return self.createDocContent();
-          },        
-          onError: function(){ 
-            alert('You must put something in the File Contents or there will be nothing to save!'); 
-          },          
-          swf: H5P.getLibraryPath('H5P.ExportableTextArea-1.0') + '/resources/downloadify.swf',
-          width: 100,
-          height: 30,
-          transparent: true,
-          append: false,
-          label: title
-        });
-        
-        var html =  '<a href="javascript:void(0)" class="h5p-eta-export flash" style="display: none;">'+$downloadify.html()+'</a>';
-        $downloadify.remove();
-        return html;
-      }
-    },
-    
-    useFileSaver: function() {
-      if(useFlash === undefined) {
-        useFlash = $.browser.msie && $.browser.version<10;
-      }
-      return !useFlash;
-    },
-    
-    /**
-     *  Check if this device/browser supports download 
-     * at all 
-     */
-    supportsExport: function() {
-      if(deviceIsIPx === undefined) {
-        deviceIsIPx = navigator.userAgent.match(/iPad/i) === null && 
-              navigator.userAgent.match(/iPhone/i) === null &&
-              navigator.userAgent.match(/iPod/i) === null;
-      }
-      return deviceIsIPx;
+      saveAs(blob, 'exported-text.doc');
     }
   };
-})();
 
+  this.createDocContent = function () {
+    // Create HTML:
+    var html = '<html><head><meta charset="UTF-8"></meta></head><body>';
+    H5P.jQuery('.h5p-slide').each(function (index) {
+      // Sort on index per slide, then create html
+      H5P.jQuery('.h5p-eta-input', this).sort(function (a, b) {
+        return H5P.jQuery(a).data('index') > H5P.jQuery(b).data('index') ? 1 : -1;
+      }).each(function () {
+        html += '<h2>' + H5P.jQuery(this).prev().find('.label').text() + '</h2>';
+        html += '<p>' + H5P.jQuery(this).val() + '</p>';
+      });
+    });
+    html += '</body></html>';
+
+    return html;
+  };
+
+  this.createExportButton = function (title) {
+    var self = this;
+
+    if (!H5P.ExportableTextArea.Exporter.supportsExport()) {
+      return '';
+    }
+
+    if (this.useFileSaver()) {
+      return '<a href="#" class="h5p-eta-export">' + title + '</a>';
+    } else {
+      var $downloadify = $('<div></div>');
+      $downloadify.appendTo('body');
+
+      $downloadify.downloadify({
+        filename: function () {
+          return 'answer-text.doc';
+        },
+        data: function () {
+          return self.createDocContent();
+        },
+        onError: function () {
+          alert('You must put something in the File Contents or there will be nothing to save!');
+        },
+        swf: H5P.getLibraryPath('downloadify-1.0') + '/resources/downloadify.swf',
+        width: 100,
+        height: 30,
+        transparent: true,
+        append: false,
+        label: title
+      });
+
+      var html = '<a href="#" class="h5p-eta-export flash">' + $downloadify.html() + '</a>';
+      $downloadify.remove();
+      return html;
+    }
+  };
+
+  this.useFileSaver = function () {
+    if (useFlash === undefined) {
+      useFlash = (navigator.userAgent.match(/MSIE 8/i) !== null || navigator.userAgent.match(/MSIE 9/i) !== null);
+    }
+    return !useFlash;
+  };
+
+  /**
+   *  Check if this device/browser supports download
+   * at all
+   */
+  this.supportsExport = function () {
+    if (deviceIsIPx === undefined) {
+      deviceIsIPx = navigator.userAgent.match(/iPad/i) === null &&
+        navigator.userAgent.match(/iPhone/i) === null &&
+        navigator.userAgent.match(/iPod/i) === null;
+    }
+    return deviceIsIPx;
+  };
+  
+  return this;
+})();
